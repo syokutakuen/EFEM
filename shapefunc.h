613dee13b84c1bbd979a38632143118443cdd6d0
食卓塩 <47070262+syokutakuen@users.noreply.github.com>
2019-02-11 09:40:36 +0900

    Create shapefunc.h
    形状関数のプロトタイプヘッダー

---
+++ shapefunc.h
@@ -0,0 +1,33 @@
+//
+//  形状関数の宣言
+//
+
+#if !defined (__SHAPEFUNC_H__)
+#define__SHAPEFUNC_H__
+
+void line1L (double *N, const double *t);
+void line2L (double *N, const double *t);
+void line3L (double *N, const double *t);
+void dline1L (double *N, const double *t);
+void dline2L (double *N, const double *t);
+void dline3L (double *N, const double *t);
+void tri1 (double *N, const double *t);
+void tri2 (double *N, const double *t);
+void tri3L (double *N, const double *t);
+void tri3S (double *N, const double *t);
+void dtri1 (double *N, const double *t);
+void dtri2 (double *N, const double *t);
+void dtri3L (double *N, const double *t);
+void tri3S (double *N, const double *t);
+void quad1 (double *N, const double *t);
+void dquad1 (double *N, const double *t);
+void quad2L (double *N, const double *t);
+void dquad2L (double *N, const double *t);
+void quad3L (double *N, const double *t);
+void dquad3 (double *N, const double *t);
+void quad2S (double *N, const double *t);
+void quad2S (double *N, const double *t);
+void quad3S (double *N, const double *t);
+void dquad3S (double *N, const double *t);
+
+#endif
