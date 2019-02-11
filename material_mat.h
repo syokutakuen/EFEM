aac0858bd32cebca9ba235c7a9792ee25391cb73
食卓塩 <47070262+syokutakuen@users.noreply.github.com>
2019-02-11 10:49:43 +0900

    Rename material_mat.c to material_mat.h

---
+++ material_mat.h
@@ -0,0 +1,10 @@
+//
+//  物性マトリックス関係
+//
+
+//  弾性物性マトリックスの作成
+void calc_De_Iso_PSS_Mat(double *De, const MatData *tab);
+void calc_De_Iso_PSN_Mat(double *De, const MatData *tab);
+void calc_De_Iso_AXSol_Mat(double *De, const MatData *tab);
+void calc_De_Iso_Solid_Mat(double *De, const MatData *tab);
+
