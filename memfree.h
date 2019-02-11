47e78a75ee7aff1faaf1ddfe5de745fec0c8bd57
食卓塩 <47070262+syokutakuen@users.noreply.github.com>
2019-02-11 23:16:39 +0900

    Create memfree.h

---
+++ memfree.h
@@ -0,0 +1,11 @@
+//
+//
+//
+
+void free_elem_data (int index);
+void free_set_data (SetData *p);
+void free_mesh_data ();
+void free_load (int ndata, LoadData *p);
+void free_bc_data ();
+void free_sys_info ();
+void free_data ();
